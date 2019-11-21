c5223f1a842b494f9636cdb13ef10727a22c866c
Mateus C <mateus.mcg@gmail.com>
2019-11-20 21:27:48 -0300

    Added basic spotify structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'spotify-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
