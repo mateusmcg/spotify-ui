27a60e4433eaf6bd38276523b171c2bb59b50595
Mateus C <mateus.mcg@gmail.com>
2019-11-21 23:31:52 -0300

    Finished side-nav

--- src/app/side-nav/side-nav.module.ts
+++ src/app/side-nav/side-nav.module.ts
@@ -4,9 +4,10 @@ import { SideNavComponent } from './side-nav.component';
 import { LogoComponent } from './logo/logo.component';
 import { MenuComponent } from './menu/menu.component';
 import { PlaylistComponent } from './playlist/playlist.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [SideNavComponent, LogoComponent, MenuComponent, PlaylistComponent],
+  declarations: [SideNavComponent, LogoComponent, MenuComponent, PlaylistComponent, FooterComponent],
   imports: [CommonModule],
   exports: [SideNavComponent]
 })
