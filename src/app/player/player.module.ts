b79880c36515a73b633fabcd0587cbdaa249566f
Mateus C <mateus.mcg@gmail.com>
2019-11-25 18:58:44 -0300

    Player left side done

--- src/app/player/player.module.ts
+++ src/app/player/player.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlayerComponent } from './player.component';
+import { MusicComponent } from './music/music.component';
+import { AudioPlayerComponent } from './audio-player/audio-player.component';
+import { ConfigsComponent } from './configs/configs.component';
 
 @NgModule({
-  declarations: [PlayerComponent],
+  declarations: [PlayerComponent, MusicComponent, AudioPlayerComponent, ConfigsComponent],
   imports: [CommonModule],
   exports: [PlayerComponent]
 })
