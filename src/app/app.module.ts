b8b707a0bf8973fe58ccd6100d55150ec05f6d7e
Mateus C <mateus.mcg@gmail.com>
2019-12-12 20:27:11 -0300

    [Done] - Profile page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideNavModule } from './side-nav/side-nav.module';
 import { PlayerModule } from './player/player.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SideNavModule, PlayerModule],
+  imports: [BrowserModule, AppRoutingModule, SideNavModule, PlayerModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
